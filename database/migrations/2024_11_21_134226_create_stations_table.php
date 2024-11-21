e80d2c05988e879a1765f02979fe84d6b4d55566
Molten-Verb <sanches-9868@yandex.ru>
2024-11-21 19:21:09 +0300

    правка миграции

--- database/migrations/2024_11_21_134226_create_stations_table.php
+++ database/migrations/2024_11_21_134226_create_stations_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name')->nullable();
             $table->timestamps();
+
+            $table->foreign('route_id')->references('id')->on('routes');
         });
     }
 
